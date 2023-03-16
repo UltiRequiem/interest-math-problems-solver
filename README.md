37c619603806ba05522f4b6fc9206a26034ea26b
Eliaz Bobadilla <ultirequiem@pm.me>
2023-03-16 12:55:53 -0500

    :recycle: Update

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-solve math problems related to interest
-
-todo: add tailwinds, add more templates, add name preferences, locales, redo with deno tech (jk), add ads(idk how, mainly to learn how), share with friends
-
-im really busy cuz exams but probably will have time in one month or so.
+# Math Intereste
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,48 @@
-import {useState} from "react"
+import {useState, ChangeEvent} from "react"
 
 function calculateInterest(capital: number, interestAnualPercentage: number, months: number) {
         return capital * (interestAnualPercentage / 100) * (months / 12)
 }
 
-export default function Home() {
+
+
+export function Box() {
         const [capital, setCapital] = useState(0)
         const [interest, setInterest] = useState(0)
         const [time, setTime] = useState(0)
 
 
         return (
-                <>
-                        <h1>Interes stuff</h1>
+                <div className="bg-blue-600 m-2 p-5 rounded-xl text-xl flex flex-col items-center justify-center">
+                        <p>Se presta <input min="1" className="w-20 h-5 mb-2" onInput={(event: ChangeEvent<HTMLInputElement>) => setCapital(+event.target.value)} type="number" />
+                                {" "} a Eliaz al <input min="1" className="w-12 h-5 mb-2" onInput={(event) => setInterest(event.target.value)} type="number" /> %
+                                anual por <input min="1" className="w-12 h-5 mb-2" onInput={(event) => setTime(event.target.value)} type="number" /> meses.
+                                ¿Cual es el interes simple que genera dicho prestamo?</p>
 
-                        <p>Cesar presta <input onInput={(event) => setCapital(event.target.value)} type="number" />
-                                a Eliaz al <input onInput={(event) => setInterest(event.target.value)} type="number" />%
-                                anual por <input onInput={(event) => setTime(event.target.value)} type="number" /> meses.
-                                Cual es el interes simple que genera dicho prestamo?</p>
+                        <p className="m-2 p-1 bg-blue-500 w-6/12 rounded-xl">El interes simple es {calculateInterest(capital, interest, time) === 0 ? "..." : calculateInterest(capital, interest, time)}</p>
 
-                        <p>Capital: {capital} </p>
-                        <p>Interes: {interest} </p>
-                        <p>Tiempo: {time} </p>
+                </div>
 
-                        <p>El interes simple es {calculateInterest(capital, interest, time)}</p>
+        )
+}
 
-                </>
+export default function Home() {
+        return (
+                <div className="h-screen flex flex-col bg-green-500">
 
+                        <div className="text-center object-fill flex-1">
+
+                                <header className="p-3 bg-green-400">
+                                        <h1 className="text-4xl m-3">Problemas de Interes</h1>
+                                </header>
+
+                                <section className="grid-flow-col gap-3 p-6">
+                                        <Box />
+                                </section>
+
+                        </div>
+
+                        <p className="text-2xl p-4 bg-yellow-200">Eliaz Bobadilla</p>
+                </div>
         )
 }
