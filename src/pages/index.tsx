f3abafad6fbe5bee11dcd36c7949d1694c9e864b
Eliaz Bobadilla <ultirequiem@pm.me>
2023-03-16 13:15:53 -0500

    :recycle: Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,10 +20,13 @@ export function Box() {
                 <div className="bg-blue-600 m-2 p-5 rounded-xl text-xl flex flex-col items-center justify-center">
                         <p>Se presta <input min="1" className="w-20 h-5 mb-2" onInput={capitalHandler} type="number" />
                                 {" "} soles a Eliaz al <input min="1" className="w-12 h-5 mb-2" onInput={interestHandler} type="number" /> %
-                                anual por <input min="1" className="w-12 h-5 mb-2" onInput={timeHandler} type="number" /> meses.
-                                ¿Cual es el interes simple que genera dicho prestamo?</p>
+                                anual por <input min="1" className="w-12 h-5 mb-2" onInput={timeHandler} type="number" /> meses. {" "}
+                                <span className="font-bold">
+                                        ¿Cual es el interes simple que genera dicho prestamo? ¿Y el monto?
+                                </span>
+                        </p>
 
-                        <p className="m-2 p-1 bg-blue-500 w-6/12 rounded-xl">El interes simple es {calculateInterest(capital, interest, time) === 0 ? "..." : `${calculateInterest(capital, interest, time).toFixed(3)} soles.`}</p>
+                        <p className="m-2 p-1 bg-blue-500 w-6/12 rounded-xl">El interes simple y el monto son respectivamente {calculateInterest(capital, interest, time) === 0 ? "..." : `${calculateInterest(capital, interest, time).toFixed(3)} y ${(calculateInterest(capital, interest, time) + capital).toFixed(3)} soles.`}</p>
                 </div>
 
         )
